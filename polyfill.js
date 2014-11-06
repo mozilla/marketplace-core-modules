603b990ae237aaf5a6cbe51a8da5c9fcfa64b8c5
Kevin Ngo <ngoke@mozilla.com>
2014-11-06 06:25:16 -0800

    bump polyfill

--- polyfill.js
+++ polyfill.js
@@ -1,8 +1,7 @@
 define('polyfill', [], function() {
     // polyfill window.location.origin only available in FF21+
-    if (!window.location.origin) {
-        window.location.origin = window.location.protocol + '//' +
-            window.location.hostname +
-            (window.location.port ? ':' + window.location.port: '');
+    var loc = window.location;
+    if (!loc.origin) {
+        loc.origin = loc.protocol + '//' + loc.host;
     }
 });
