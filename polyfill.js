aa93281b6371dc82edd3943e69545527ef0f500e
Christopher Van <cvan@mozilla.com>
2014-11-18 12:05:35 -0800

    add `window.performance.now` polyfill (bug 1090572)

--- polyfill.js
+++ polyfill.js
@@ -1,7 +1,19 @@
 define('polyfill', [], function() {
-    // polyfill window.location.origin only available in FF21+
+    // Polyfill `window.location.origin` (available in only FF 21+).
     var loc = window.location;
     if (!loc.origin) {
         loc.origin = loc.protocol + '//' + loc.host;
+    }
+
+    // Polyfill `window.performance.now`
+    // (available in only IE 10+, FF 15+, Chrome 20+, PhantomJS 2).
+    if (!window.performance) {
+        window.performance = {};
+    }
+    if (!window.performance.now) {
+        window.performance.now = function() {
+            // Avoid using `Date.now` for IE 8 and lower.
+            return +new Date();
+        };
     }
 });
