431a5338d5e542699eead4d35df143844dfca566
Mark Striemer <mstriemer@mozilla.com>
2015-02-03 10:46:02 -0600

    Add failing test

--- tests/cache.js
+++ tests/cache.js
@@ -13,7 +13,7 @@ define('tests/cache', ['assert', 'underscore', 'cache', 'settings', 'user'], fun
             var key = 'test:thisisatest';
             var str = 'foobartest';
             assert(!cache.has(key));
-            cache.set(key, str);
+            cache.set(key, str + 'foo');
             assert(cache.has(key));
             eq_(cache.get(key), str);
             assert(cache.has(key));
