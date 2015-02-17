cd817da729462125c0e2fd02a2736ec18796c67b
Mark Striemer <mstriemer@mozilla.com>
2015-02-17 15:21:32 -0600

    Remove old comment from core/init

--- core/init.js
+++ core/init.js
@@ -1,11 +1,3 @@
-/*
-   init.js is appending to our minified bundles in the CP build process.
-   Pulls in the settings.js module once it is available.
-   Looks for settings.init_module which points to the module name of our
-   main/initialization module.
-   Invokes the main/initialization module once it is available to kick things
-   off.
-*/
 define('core/init',
     ['core/defer', 'core/router', 'core/utils'],
     function(defer, router, utils) {
