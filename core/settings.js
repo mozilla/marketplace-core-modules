94733b434b754e4535caffe272874cce141ca622
Mark Striemer <mstriemer@mozilla.com>
2015-02-18 19:58:57 -0600

    Use urls.cdn_url() instead of settings.cdn_url

--- core/settings.js
+++ core/settings.js
@@ -1,7 +1,6 @@
 define('core/settings', ['underscore'], function(_) {
     var settings = {
         _extend: function(extra_settings) {
-            // Hopefully `_extend` isn't in `extra_setting` mirite?
             _.extend(settings, extra_settings);
         },
         switches: [],
