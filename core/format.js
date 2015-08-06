e1dbd2ebd6be9237db4329bdceb2827dceb8879a
Mark Striemer <mstriemer@mozilla.com>
2015-08-06 12:32:44 -0500

    Lazy string support for format.format

--- core/format.js
+++ core/format.js
@@ -9,21 +9,35 @@
 
 define('core/format', [], function() {
     var re = /\{([^}]+)\}/g;
+
     function format(s, args) {
         if (!s) {
             throw "Format string is empty!";
+        } else if (!args) {
+            return;
         }
-        if (!args) return;
-        if (!(args instanceof Array || args instanceof Object))
+        if (!(args instanceof Array || args instanceof Object)) {
             args = Array.prototype.slice.call(arguments, 1);
-        return s.replace(re, function(_, match){ return args[match]; });
+        }
+        var replace = function(str) {
+            return str.replace(re, function(_, match){ return args[match]; });
+        };
+        if (typeof s === 'string') {
+            // If we got a string format it now.
+            return replace(s);
+        } else {
+            // If it isn't a string, it's probably a lazy string, be lazy.
+            return {toString: function() { return replace(s.toString()); }};
+        }
     }
+
     function template(s) {
         if (!s) {
             throw "Template string is empty!";
         }
         return function(args) { return format(s, args); };
     }
+
     return {
         format: format,
         template: template
