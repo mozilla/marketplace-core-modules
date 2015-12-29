6108263582094edfafb126f6c7c258387cfec4de
tieu <tieu@users.noreply.github.com>
2015-12-29 12:46:19 -0500

    Update forms.js
    Fixed variable

--- core/forms.js
+++ core/forms.js
@@ -2,9 +2,9 @@ define('core/forms', ['core/z'], function(z) {
 
     function checkValid(form) {
         if (form) {
-            var currentForm = $(form);
-            currentForm.find('textarea').val($.trim(currentForm.find('textarea').val()) === '' ? '' : currentForm.find('textarea').val());
-            currentForm.filter(':not([novalidate])').find('button[type=submit]').attr('disabled', !form.checkValidity());
+            var $currentForm = $(form);
+            $currentForm.find('textarea').val($.trim($currentForm.find('textarea').val()) === '' ? '' : $currentForm.find('textarea').val());
+            $currentForm.filter(':not([novalidate])').find('button[type=submit]').attr('disabled', !form.checkValidity());
         }
     }
     // Note 'input' event is required for FF android (bug 977642)
